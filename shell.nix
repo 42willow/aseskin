01bb71be24d934aac4d0b12e9314f6beb6916125
willow <42willow@pm.me>
2024-12-04 14:21:49 +1100

    refactor: update to template

--- shell.nix
+++ shell.nix
@@ -1,39 +1,27 @@
 let
   pkgs = import <nixpkgs> { };
+in
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    pkg-config
+    gobject-introspection
+    cargo
+    cargo-tauri
+    nodejs
+  ];
 
-  libraries = with pkgs;[
-    webkitgtk
-    gtk3
+  buildInputs = with pkgs;[
+    at-spi2-atk
+    atkmm
     cairo
     gdk-pixbuf
     glib
-    dbus
-    openssl_3
-    librsvg
-  ];
-
-  packages = with pkgs; [
-    pkg-config
-    dbus
-    openssl_3
-    glib
     gtk3
-    libsoup
-    webkitgtk_4_1
-    appimagekit
+    harfbuzz
     librsvg
-    rustc
-    nodejs_20
-    cargo
-    rustup
+    libsoup_3
+    pango
+    webkitgtk_4_1
+    openssl
   ];
-in
-pkgs.mkShell {
-  buildInputs = packages;
-
-  shellHook =
-    ''
-      export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
-      export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
-    '';
 }
