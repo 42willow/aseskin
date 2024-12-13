76ae16e5edc3c148edc36c5906a52a7ee3df27ba
willow <42willow@pm.me>
2024-12-14 09:45:42 +1100

    chore: use flake

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs {
+            system = system;
+          };
+
+          # runtime deps
+          libraries = with pkgs; [
+            at-spi2-atk
+            atkmm
+            cairo
+            gdk-pixbuf
+            glib
+            gtk3
+            harfbuzz
+            librsvg
+            libsoup_3
+            pango
+            webkitgtk_4_1
+            openssl
+          ];
+
+          # compile-time deps
+          packages = with pkgs; [
+            pkg-config
+            gobject-introspection
+            cargo
+            cargo-tauri
+            nodejs
+          ];
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = packages;
+            buildInputs = libraries;
+            # shellHook = ''
+            #   LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+            #   XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
+            #   GIO_MODULE_DIR=${pkgs.glib-networking.out}/lib/gio/modules/
+            #   GIO_EXTRA_MODULES=${pkgs.glib-networking.out}/lib/gio/modules/
+            # '';
+          };
+        }
+      );
+}
